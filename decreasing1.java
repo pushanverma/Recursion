338c45a4992b7ed91899906ed7f4002debcdbc06
pushanverma <pushanverma.99@gmail.com>
2021-07-31 12:56:20 +0800

    adding decreasing1

---
+++ decreasing1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class decreasing1 {
+    public static void dec(int n )
+    {
+        if(n==0)
+        {
+            return;
+        }
+        System.out.println(n);
+        dec(n-1);
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner scn =new Scanner(System.in);
+        int n =scn.nextInt();
+        dec(n);
+    }
+}
